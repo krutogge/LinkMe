d6f0d857ca376fd7e86a5e5d69d67265e91f4704
krutogge <91694984+krutogge@users.noreply.github.com>
2025-03-08 13:17:00 +0100

    Update setup.py
    undetected chrome choses its own default, just guide it

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #this is the setup data needed to utilize linkedin auto-profil visiter LinkMe
-USER_DATA_DIR = '...Your pathway...config/google-chrome/Default'
+USER_DATA_DIR = '...Your pathway...config/google-chrome'
 USERNAME = 'Youremail@gmail.com'
 PASSWORD = 'Yourpassword'
